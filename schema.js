f5bff67d9d004dfbf37aee49ad1463afd160117d
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-11-13 18:18:54 +0530

    define launches field query :construction:

--- schema.js
+++ schema.js
@@ -1,4 +1,13 @@
-const { GraphQLObjectType, GraphQLInt, GraphQLBoolean, GraphQLString }= require('graphql');
+const { 
+  GraphQLObjectType, 
+  GraphQLInt, 
+  GraphQLBoolean, 
+  GraphQLString, 
+  GraphQLList, 
+  GraphQLSchema 
+} = require('graphql');
+
+const axios = require('axios');
 
 // Launch Type
 const LaunchType = new GraphQLObjectType({
@@ -17,8 +26,26 @@ const LaunchType = new GraphQLObjectType({
 const RocketType = new GraphQLObjectType({
     name: 'Rocket',
     fields: () => ({
-    rocket_id: { type: GraphQLBString },
-    rocket_name: { type: GraphQLBString },
-    rocket_type: { type: GraphQLBString }
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString },
+    rocket_type: { type: GraphQLString }
     })
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        launches: {
+            type: new GraphQLList(LaunchType),
+            resolve(parent, args){
+                return axios.get('https://api.spacexdata.com/v3/launches')
+                .then(res => res.data);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
