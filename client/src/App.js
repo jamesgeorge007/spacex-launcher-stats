ed95ec6d5ad5d942a40e7493c8edb9d9ca2c3305
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-11-13 21:59:57 +0530

    bring about Launches component :+1:

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
+import Launches from './components/Launches';
 import ApolloClient from 'apollo-boost';
-import ApolloProvider from 'react-apollo';
+import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql'
@@ -14,6 +15,7 @@ class App extends Component {
       <ApolloProvider client={client}>
       <div className="container">
         <img src={logo} style={{width: '30vw', display: 'block', margin: 'auto'}} alt="SpaceX"/>
+        <Launches />
       </div>
       </ApolloProvider>
     );
--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,9 +1,25 @@
-import React, { Component } from react;
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const LAUNCHES_QUERY = gql`
+    query LaunchesQuery{
+      launches{
+        flight_number
+        mission_name
+        launch_date_local
+        launch_success    
+        }
+    }
+`;
 
 class Launches extends Component{
     render(){
-        <div>
-        </div>
+        return(
+            <div>
+                <h1 className="display-4 my-3">Launches</h1>
+            </div>
+        );
     }
 }
 
