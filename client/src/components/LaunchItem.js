f2b37c26d331394640189f0082230996e13bf39e
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-11-13 22:41:12 +0530

    render launches properly :+1:

--- client/src/components/LaunchItem.js
+++ client/src/components/LaunchItem.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-const LaunchItem = (props) => {
-    console.log(props.launch);
+const LaunchItem = ({launch: {flight_number, mission_name, launch_date_local, launch_success}}) => {
     return (
-    <div>
-      Test
+    <div className="card card-body mb-3">
+      <div className="row">
+        <div className="col-md-9">
+            <h4>Mission: {mission_name}</h4>
+            <p>Date: {launch_date_local} </p>
+        </div>
+        <div className="col-md-3">
+            <button className="btn btn-secondary">View Details</button>
+        </div>
+      </div>
     </div>
   )
 }
