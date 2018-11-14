305db6335726591b80b39ad839f37c3d79fc3c16
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-11-14 20:32:36 +0530

    colorise launch titles :+1:

--- client/src/components/LaunchItem.js
+++ client/src/components/LaunchItem.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import classNames from 'classnames';
 
 const LaunchItem = ({launch: {flight_number, mission_name, launch_date_local, launch_success}}) => {
     return (
     <div className="card card-body mb-3">
       <div className="row">
         <div className="col-md-9">
-            <h4>Mission: {mission_name}</h4>
+            <h4>Mission: 
+              <span className={classNames({
+              'text-success': launch_success,
+              'text-danger': !launch_success
+            })}>
+            {mission_name}
+            </span>
+            </h4>
             <p>Date: {launch_date_local} </p>
         </div>
         <div className="col-md-3">
